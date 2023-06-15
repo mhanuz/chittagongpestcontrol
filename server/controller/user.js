11a33970fff0e043a8dac0b7ce924809995e40bd
Anuz <ceanuz66@gmail.com>
2023-06-15 10:16:13 +0600

    add sign up

--- server/controller/user.js
+++ server/controller/user.js
@@ -16,3 +16,19 @@ export const signin = async(req, res) => {
         res.status(500).json({msg:'something went wrong'})
     }
 }
+
+export const signup = async(req, res)=>{
+    const {email, password, confirmPassword, firstName, lastName} = req.body;
+
+    try {
+        const existingUser = await User.findOne({email});
+        if(existingUser) return res.status(400).json({msg:'User already exists.'})
+        if(password !== confirmPassword) return res.status(400).json({msg:'Password don\'t match.'})
+        const hashPassword = await bcrypt.hash(password, 12);
+        const result = User.create({email, password:hashPassword, name:`${firstName} ${lastName}`})
+        const token = jwt.sign({email: result.email, id:result._id},'test',{expiresIn:'1h'});
+        res.status(200).json({result,token});
+    } catch (error) {
+        res.status(500).json({msg:'Something went wrong.'})
+    }
+}
--- server/index.js
+++ server/index.js
@@ -18,6 +18,7 @@ app.use(bodyParser.json({ limit:'30mb', extended:true}));
 app.use(bodyParser.urlencoded({ limit:'30mb', extended:true}));
 app.use(cors());
 
+// APIs
 app.use('/posts', postRoutes);
 app.use('/user', userRoutes)
 
